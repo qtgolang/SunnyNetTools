e4e63e8927aef8529f9d207432c8b68f439799b2
Qin tian <57537577+qtgolang@users.noreply.github.com>
2026-05-22 17:26:54 +0800

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,7 @@
+taskkill /F /IM node.exe
+taskkill /F /IM wails3.exe
+taskkill /F /IM SunnyNetTools.exe
+set GOEXPERIMENT=nodwarf5
+set CGO_ENABLED=1
+rd /s /q frontend\node_modules\.vite
+wails3 build
